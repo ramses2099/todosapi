66df6a7e91a3f8bcd7087af0b26f7ea12853ed77
ramses2099 <ramses2099@gmail.com>
2024-06-23 09:49:51 -0400

    last commit

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i#ri$g4svbis7wz*762uxi$vir4)gc*kj(mh_3e6iuek9hyyuz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
